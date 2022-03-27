4afc39bc631994ebd40cbdfcc48a7092f7a08f0f
thomashsimm <thomas.simm@gmail.com>
2022-03-27 14:54:42 +0100

    nlp sentiment analysis streamlit app

---
+++ nlpsentiment.py
@@ -0,0 +1,28 @@
+from fastbook import *
+import streamlit as st
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+
+@st.cache(allow_output_mutation=True)
+def loads():
+    learner = load_learner('IMDB_class_model_export.pkl')
+    return learner
+
+learner = loads()
+txta = st.text_area('Enter text') 
+
+ll = learner.predict(txta)
+
+scora = 100-int(ll[2][0]*100)
+sent = ll[0]
+if sent=='pos':
+    sent='Positive'
+elif sent=='neg':
+    sent='Negative'
+else:
+    sent='??'
+
+st.title("Rating =  {}  / 100".format(scora))
+st.title("Overall sentiment is {}".format(sent) )
