b52f98ade9fa83770c893922dbb8837628477870
thomashsimm <thomas.simm@gmail.com>
2022-03-27 20:31:26 +0100

    Update nlpsentiment.py

--- nlpsentiment.py
+++ nlpsentiment.py
@@ -1,8 +1,6 @@
 from fastbook import *
 import streamlit as st
-
 from os.path import exists
-# import gdown
 
 #next bit needed for windows
 # import pathlib
@@ -16,29 +14,30 @@ def loads():
     filename = 'IMDB_class_model_export.pkl'
     file_exists = exists(filename)
 
+    # if file doesn't exist load it from GitHub
     if not file_exists:
         with st.spinner("Downloading model... this may take a while! \n Don't stop it!"):
 
             import urllib
 
-
+            # file more than 200MB so added as a realease on github
+            # url is https://github.com/spacebluebamboo/nlp_sentiment_streamlit/releases/tag/V2
+            # so modified slightly and included file name
             url='https://github.com/spacebluebamboo/nlp_sentiment_streamlit/releases/download/V2/IMDB_class_model_export.pkl'
             
 
             urllib.request.urlretrieve(url, filename)
         return []
+    #if it does create the learner
     else:
         learner = load_learner(filename)
         return learner
         
 
-
+# Load the data
 learner=loads()
 
-filename = 'IMDB_class_model_export.pkl'
-file_exists = exists(filename)
-file_exists
-
+# Stuff on streamlit
 txta = st.text_area('Enter text') 
 
 ll = learner.predict(txta)
