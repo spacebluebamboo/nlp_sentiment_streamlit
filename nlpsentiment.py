768d52f8bf2498ec2b8d30cd84e50fc8e020bdfa
thomashsimm <thomas.simm@gmail.com>
2022-03-27 16:47:27 +0100

    Update nlpsentiment.py

--- nlpsentiment.py
+++ nlpsentiment.py
@@ -13,7 +13,7 @@ def loads():
     save_dest = Path('model')
     save_dest.mkdir(exist_ok=True)
     
-    f_checkpoint = Path("model/IMDB_class_model_export.pklt")
+    f_checkpoint = Path("model/IMDB_class_model_export.pkl")
 
     if not f_checkpoint.exists():
         with st.spinner("Downloading model... this may take a while! \n Don't stop it!"):
@@ -24,14 +24,14 @@ def loads():
             url='https://github.com/spacebluebamboo/nlp_sentiment_streamlit/releases/download/V2/IMDB_class_model_export.pkl'
             filename =  url.split('/')[-1]
 
-            urllib.request.urlretrieve(url, filename)
+            urllib.request.urlretrieve(url, f_checkpoint)
 #             url = 'https://drive.google.com/uc?id=1F3Qi5s4FJiRGC5hBrAoOpKBLJ9HzvAft'
 # #             output = f_checkpoint#'IMDB_class_model_export.pkl'
 # #             gdown.download(url, output, quiet=False)
 #             from GD_download import download_file_from_google_drive
 #             download_file_from_google_drive(url, f_checkpoint)
-    learner = load_learner(f_checkpoint, map_location=device)
-    return learner
+        learner = load_learner(f_checkpoint, map_location=device)
+        return learner
 
 learner = loads()
 txta = st.text_area('Enter text') 
